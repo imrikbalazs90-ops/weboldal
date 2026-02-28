e8b32fecc84b6248580b3ceba7aab19ebf196ed8
imrikbalazs90-ops <imrikbalazs90@gmail.com>
2026-02-28 23:00:04 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -57,3 +57,22 @@ link.classList.add("active");
 });
 
 }
+/* ================= WOW PARALLAX ================= */
+
+document.addEventListener("mousemove",(e)=>{
+
+const glows=document.querySelectorAll(".glow");
+
+const x=(e.clientX/window.innerWidth-0.5)*40;
+const y=(e.clientY/window.innerHeight-0.5)*40;
+
+glows.forEach((g,i)=>{
+
+const speed=(i+1)*0.4;
+
+g.style.transform=
+`translate(${x*speed}px, ${y*speed}px)`;
+
+});
+
+});
