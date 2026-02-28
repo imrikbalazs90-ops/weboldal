37d266c020326379d5d1b967a195a85bd1edc075
imrikbalazs90-ops <imrikbalazs90@gmail.com>
2026-02-28 21:58:56 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,44 @@
+/* ================= SCROLL TOP ================= */
+
 function scrollTopSmooth(){
 window.scrollTo({top:0,behavior:'smooth'});
 }
+
+/* ================= PAGE TRANSITION ================= */
+
+document.addEventListener("DOMContentLoaded",()=>{
+
+const links=document.querySelectorAll("a[href]");
+
+links.forEach(link=>{
+
+const url=link.getAttribute("href");
+
+/* csak belső oldalak */
+
+if(
+url &&
+!url.startsWith("#") &&
+!url.startsWith("http")
+){
+
+link.addEventListener("click",function(e){
+
+e.preventDefault();
+
+document.body.classList.add("page-leave");
+
+setTimeout(()=>{
+window.location.href=url;
+},350);
+
+});
+}
+
+});
+
+/* belépő animáció */
+
+document.body.classList.add("page-enter");
+
+});
