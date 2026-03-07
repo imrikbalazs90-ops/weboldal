b3fa79118b467dab075e5b64d32e4bc0191c495d
Balazs Imrik <imrikbalazs90@gmail.com>
2026-03-07 21:40:05 +0100

    fix

--- js/main.js
+++ js/main.js
@@ -4,3 +4,26 @@ fetch(window.location.origin + '/components/navbar.html')
     document.getElementById("navbar").innerHTML = data;
 })
 .catch(error => console.error("Navbar load error:", error));
+
+fetch('/components/navbar.html')
+.then(response => response.text())
+.then(data => {
+
+document.getElementById("navbar").innerHTML = data;
+
+/* aktív menü */
+
+const links = document.querySelectorAll(".nav-link");
+const current = window.location.pathname;
+
+links.forEach(link => {
+
+if (current.includes(link.getAttribute("href"))) {
+
+link.classList.add("active");
+
+}
+
+});
+
+});
